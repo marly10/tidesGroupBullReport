8efba488f9cc51f43a680a8ee6adea98db6c4c51
marly10 <mckmarly@icloud.com>
2020-05-30 00:10:36 -0700

    new data 2.0

--- bullReport.py
+++ bullReport.py
@@ -1,6 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask
 
+
+
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+
+instructions2 = '''
+    <p><em>Hint</em>: Me adding new filesSSS.</p>\n'''
+
+footer_text = '</body>\n</html>'
 
 page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -12,5 +32,21 @@ period = tonight.find(class_="period-name").get_text()
 short_desc = tonight.find(class_="short-desc").get_text()
 temp = tonight.find(class_="temp").get_text()
 
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
 
-print(period, short_desc, temp)
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + instructions2 + footer_text  + period +'\n'+ short_desc +'\n' +temp))
+
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+application.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
