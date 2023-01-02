cfaf7ce6851c3e29793b24583749fcdeabfbeff6
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2023-01-02 00:26:49 -0800

    Add files via upload

---
+++ slack.py
@@ -0,0 +1,14 @@
+import requests, json
+
+def send_slack_message(payload, webhook):
+    """Send a Slack message to a channel via a webhook. 
+    
+    Args:
+        payload (dict): Dictionary containing Slack message, i.e. {"text": "This is a test"}
+        webhook (str): Full Slack webhook URL for your chosen channel. 
+    
+    Returns:
+        HTTP response code, i.e. <Response [503]>
+    """
+
+    return requests.post(webhook, json.dumps(payload))
