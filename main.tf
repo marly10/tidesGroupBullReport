6b22ace95c01e678648c03ef7956306ece574c0b
Tricia McKenzie <triciamckenzie@Tricias-MacBook-Pro.local>
2023-01-09 21:30:57 -0800

    test-poc

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 provider "aws" {
-  region                  = "us-east-1"
-  profile                 = "default"
+     region                  = "us-east-1"
+
 }
 
-resource "aws_instance" "ec2_instances" {
-    ami = "ami-08fdec01f5df9998f"
-    subnet_id = "subnet-090cb74e9416cbcbd"
-    instance_type = "t2.micro"
-    key_name = "t_mac"
+resource "aws_instance" "ec2_example" {
+      ami = "ami-08fdec01f5df9998f"
+    instance_type = "t2.micro" 
+     key_name = "t_mac"
+    tags = {
+        Name = "Terraform EC2 1101"
+    }
 }
