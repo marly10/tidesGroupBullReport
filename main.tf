1088840297ece91d074d29ef7c366ba786bd4174
Tricia McKenzie <triciamckenzie@Tricias-MacBook-Pro.local>
2023-01-10 22:56:59 -0800

    test-poc-6

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2_exampl" {
       ami = "ami-08fdec01f5df9998f"
+      iam_instance_profile = "gha_poc"
      instance_type = "t2.micro" 
      key_name = "t_mac"
     tags = {
