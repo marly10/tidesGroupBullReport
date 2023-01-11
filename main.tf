e2ea353d8c647f09fad2d711fe149336f8fedc36
Tricia McKenzie <triciamckenzie@Tricias-MacBook-Pro.local>
2023-01-10 22:35:35 -0800

    test-poc-3

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = "AKIA2XM3C3RHA3KWWPFG"
-  secret_key = "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"
+shared_credentials_file = "~/.aws/credentials"
     region = "us-east-1"
 }
 
 resource "aws_instance" "ec2_exampl" {
       ami = "ami-08fdec01f5df9998f"
+      iam_instance_profile = "gha_poc"
      instance_type = "t2.micro" 
      key_name = "t_mac"
     tags = {
